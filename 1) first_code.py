c409c84313ff751ce6418c86508cb5632a2a46a0
Yashraj Pahuja <yashrajpahuja9999@gmail.com>
2024-12-30 19:17:20 +0530

    first code

---
+++ 1) first_code.py
@@ -0,0 +1,14 @@
+'''PYTHON BASIC PROGRAMS'''
+
+
+print("hello world")   #string
+print("this is a string")  #string
+print(10)        #integer
+print("10")     # integer
+print("i love python")   # string
+print(10+12)  # "+" for addition
+print(25*2)   # "*" for multiplication
+print(87-76)  # "-" for subtraction
+print(45/4)   # "/" for divison
+print(45%4)   # "%"  gives remainder
+print(45//4)  # "//" gives quoteint before the decimal
