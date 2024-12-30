0abc71b9bbb39ac459092a02520119f346706ced
Yashraj Pahuja <yashrajpahuja9999@gmail.com>
2024-12-31 02:45:27 +0530

    added second file

---
+++ 2 day.py
@@ -0,0 +1,27 @@
+'''comments ,escape sequence and print in python'''
+
+print("hey im a good boy")
+#print statement is used for execution of code
+#<---- this hastag is used for commenting in python
+
+'''to select multiple lines as a comment use shortcut 
+CTRL + / '''
+
+print("example")
+print("# Yashraj Pahuja \n # Future Founder & CEO Of AERONOX And DRONPROGGS")
+
+'''(escape sequence) \n is used to execute code in next line'''
+# Yashraj Pahuja
+# Future Founder & CEO Of AERONOX And DRONPROGGS
+'''to uncomment use same shortcut CTRL+/'''
+
+
+"""
+short cut ---> ALT + Down arrow/Up arrow .use to move line 
+
+"""
+print("this is will be executed") #error
+print("this will be \"executed'")
+
+'''"sep" is use to seperate each element in string,list...(learn more in upcoming files) '''
+print("hey",5,8,sep="*")
