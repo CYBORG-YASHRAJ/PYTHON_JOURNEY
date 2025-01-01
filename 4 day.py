1bcd6554ccab88ef91520e133ac37a982fd8d8b1
Yashraj Pahuja <yashrajpahuja9999@gmail.com>
2025-01-02 01:17:43 +0530

    added fourth file

---
+++ 4 day.py
@@ -0,0 +1,31 @@
+'''OPERATORS'''
+# ADDITION +
+# SUBTRACTION -
+# MULTIPLY *
+# DIVISION /
+# MODULOS %
+# FLOOR DIVISION //
+# EXPONENTIAL **
+
+"""examples"""
+print(15+6)
+print(15-7)
+print(15*7)
+print(15/7)
+print(15%7)
+print(15//7)
+print(5**3)
+
+'''create a basic calculator '''
+m=int(input("enter the first number"))   # int function is used to take input of numbers only,otherwise it gives error
+n=int(input("enter the second number"))
+ans1=m+n
+print("addition of two numbers is",ans1)
+ans2=m-n
+print("subtracion of two numbers is",ans2)
+ans3=m*n
+print("multiplication of two numbers is",ans3)
+ans4=m/n
+print("division of two numbers is",ans4)
+
+'''easy code'''
