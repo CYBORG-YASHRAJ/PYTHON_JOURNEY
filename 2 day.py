b4d2abebf775bb188d3653559cac9af1a6e2c0cf
Yashraj Pahuja <yashrajpahuja9999@gmail.com>
2025-01-02 02:16:36 +0530

    added fifth file

--- 2 day.py
+++ 2 day.py
@@ -20,8 +20,10 @@ print("# Yashraj Pahuja \n # Future Founder & CEO Of AERONOX And DRONPROGGS")
 short cut ---> ALT + Down arrow/Up arrow .use to move line 
 
 """
-print("this is will be executed") #error
+print("this is will be "executed") #error
 print("this will be \"executed'")
 
 '''"sep" is use to seperate each element in string,list...(learn more in upcoming files) '''
 print("hey",5,8,sep="*")
+
+"""next file """
