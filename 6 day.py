b6b237f90cf8b1fe0d37daecaf52afa057c63ba2
Yashraj Pahuja <yashrajpahuja9999@gmail.com>
2025-01-03 23:48:22 +0530

    sixth file added

---
+++ 6 day.py
@@ -0,0 +1,13 @@
+'''Taking User Input'''
+
+# basic program
+
+a=input("enter your name:")
+print("my name is",a)
+
+x=input("enter first number")
+y=input("enter your second number")
+print(x+y)
+print(int(x)+int(y))
+
+#in this above case number is entered as a string,result will be -both the string will be concatenated
